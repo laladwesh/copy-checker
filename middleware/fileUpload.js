9120ed60c6f0b939b7732190c653be1e81c10fc0
laladwesh <guptaavinash302@gmail.com>
2025-07-05 11:21:11 +0530

    added fxes

---
+++ middleware/fileUpload.js
@@ -0,0 +1,14 @@
+// middleware/fileUpload.js
+const multer  = require('multer');
+const storage = multer.memoryStorage();
+
+const fileFilter = (req, file, cb) => {
+  // allow images
+  if (file.mimetype.startsWith('image/')) return cb(null, true);
+  // allow PDFs
+  if (file.mimetype === 'application/pdf') return cb(null, true);
+  // otherwise reject
+  cb(new Error('Only image or PDF files are allowed'), false);
+};
+
+module.exports = multer({ storage, fileFilter });
