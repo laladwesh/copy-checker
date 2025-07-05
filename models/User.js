79f77ab9f36869973c5e16ed40327381fcd8995c
laladwesh <guptaavinash302@gmail.com>
2025-07-05 11:21:39 +0530

    fixxes

---
+++ models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  email:     { type: String, required: true, unique: true },
+  googleId:  { type: String },
+  role:      { type: String, enum: ['admin','examiner','student'], required: true },
+  batch:     { type: String },    // only for students
+  gender:    { type: String },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('User', UserSchema);
