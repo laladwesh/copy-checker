ebdfaece8a16d18557915494e0900903f13d34ba
laladwesh <guptaavinash302@gmail.com>
2025-07-05 11:17:50 +0530

    added changes

---
+++ config/passport.js
@@ -0,0 +1,34 @@
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User           = require('../models/User');
+
+module.exports = (passport) => {
+  passport.use(new GoogleStrategy({
+      clientID:     process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL:  process.env.GOOGLE_OAUTH_CALLBACK_URL
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const email = profile.emails[0].value;
+        let user = await User.findOne({
+          $or: [{ googleId: profile.id }, { email }]
+        });
+        if (!user) {
+          return done(null, false, { message: 'Account not registered' });
+        }
+        if (!user.googleId) {
+          user.googleId = profile.id;
+          await user.save();
+        }
+        return done(null, user);
+      } catch (err) {
+        return done(err, false);
+      }
+    }
+  ));
+
+  passport.serializeUser((user, done) => done(null, user.id));
+  passport.deserializeUser((id, done) =>
+    User.findById(id).then(u => done(null, u)).catch(done)
+  );
+};
