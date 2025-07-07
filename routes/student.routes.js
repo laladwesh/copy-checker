5369172527b12cce33added0a9486541a3eb993e
laladwesh <guptaavinash302@gmail.com>
2025-07-08 01:37:25 +0530

    made change

--- routes/student.routes.js
+++ routes/student.routes.js
@@ -1,17 +1,24 @@
-const express = require("express");
+// routes/student.routes.js
+const express = require('express');
 const {
   listCopies,
   getCopy,
   raiseQuery,
-} = require("../controllers/student.controller");
-const { ensureAuthenticated, ensureRole } = require("../middleware/auth");
-const { verifyToken } = require("../middleware/jwtAuth");
+  listQueries, // NEW: Import listQueries
+} = require('../controllers/student.controller');
+const { verifyToken } = require('../middleware/jwtAuth');
+const { ensureRole } = require('../middleware/auth');
+
 const router = express.Router();
 
-router.use(verifyToken, ensureRole("student"));
+router.use(verifyToken, ensureRole('student'));
 
-router.get("/copies", listCopies);
-router.get("/copies/:id", getCopy);
-router.post("/copies/:id/query", raiseQuery);
+// Copy Management
+router.get('/copies', listCopies);
+router.get('/copies/:id', getCopy);
+
+// Query Management
+router.post('/copies/:id/queries', raiseQuery); // Student raises query on a copy
+router.get('/queries', listQueries); // NEW: Route to list queries for the student
 
 module.exports = router;
