824cc0d09f71a7c5380e40405b0381350f528c21
laladwesh <guptaavinash302@gmail.com>
2025-07-08 10:25:49 +0530

    config fixxes in model

--- models/User.js
+++ models/User.js
@@ -1,12 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  email:     { type: String, required: true, unique: true },
-  googleId:  { type: String },
-  role:      { type: String, enum: ['admin','examiner','student'], required: true },
-  batch:     { type: String },    // only for students
-  gender:    { type: String },
-  createdAt: { type: Date, default: Date.now }
+  email: { type: String, required: true, unique: true },
+  googleId: { type: String },
+  role: {
+    type: String,
+    enum: ["admin", "examiner", "student"],
+    required: true,
+  },
+  batch: { type: String }, // only for students
+  gender: { type: String },
+  createdAt: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
