fee3c67dc6fa3cbf14b674b539360ceac192fee4
laladwesh <guptaavinash302@gmail.com>
2026-01-06 17:37:25 +0530

    app.js fixed for UI

--- client/src/App.js
+++ client/src/App.js
@@ -47,7 +47,7 @@ export default function App() {
     <div className="min-h-screen flex flex-col bg-gray-50">
       <Navbar user={user} onLogout={handleLogout} />
 
-      <main className="flex-1 container mx-auto p-4">
+      <main className=" p-4">
         <Routes>
           <Route
             path="/"
