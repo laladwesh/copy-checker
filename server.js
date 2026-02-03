904d97fadc96aa09da5557d4ae87b1c83184b029
laladwesh <guptaavinash302@gmail.com>
2026-02-03 17:10:27 +0530

    Improve documentation : server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const app = require("./app");
-const { initializeScheduledJobs } = require("./scheduledJobs");
+// const { initializeScheduledJobs } = require("./scheduledJobs");
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
   
   // Initialize scheduled jobs for automatic copy reallocation
-  initializeScheduledJobs();
+  // initializeScheduledJobs();
 });
