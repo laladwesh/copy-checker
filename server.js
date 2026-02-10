5aba8ebb00545542830c85338196ab0b7f167d73
laladwesh <guptaavinash302@gmail.com>
2026-02-11 01:07:57 +0530

    fixes

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
   
-  // Initialize scheduled jobs for automatic copy reallocation
+  // Initialize scheduled jobs for automatic reallocation
   // initializeScheduledJobs();
 });
