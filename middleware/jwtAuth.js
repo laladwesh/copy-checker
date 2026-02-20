c8a56323d960a426c3afbe7780c9fe4a26742af2
laladwesh <guptaavinash302@gmail.com>
2026-02-20 12:11:10 +0530

    Log user email in token verification process

--- middleware/jwtAuth.js
+++ middleware/jwtAuth.js
@@ -11,6 +11,7 @@ exports.verifyToken = (req, res, next) => {
   const token = auth.split(" ")[1];
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(payload.email);
     // attach user info to req; you may want to fetch full user from DB
     req.user = { _id: payload.sub, role: payload.role };
     next();
